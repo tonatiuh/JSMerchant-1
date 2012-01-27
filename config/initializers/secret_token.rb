2a1d4a88359c2f7f6c2d83bb2c1baa41e74ccafc
Phil Aquilina <philaquilina@gmail.com>
2012-01-26 17:06:46 -0800

    getting a new project going

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JSMerchant::Application.config.secret_token = 'fdeb0dcfa43024e057eccbf23ceaf22e0521e85beb82c498954333e2e6e58ea04f87243fe4e5891e2937225a82c2d7687c1dee5b031ce40a5bca9f75864f22e0'
