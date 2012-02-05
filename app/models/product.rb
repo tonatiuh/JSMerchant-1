e9fd43001dea8529ffc476d2ef092afbdd161be9
Phil Aquilina <philaquilina@gmail.com>
2012-02-05 12:31:05 -0800

    small tweak to model and show view

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,9 @@ class Product < ActiveRecord::Base
   attr_accessible :title, :price, :description, :image_url
 
   validates_numericality_of :price
+
+  def price=(input)
+    input.delete!("$")
+    super
+  end
 end
