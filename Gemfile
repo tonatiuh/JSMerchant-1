b60a2624bd401def86a68b1c19d719a31c9d479e
Phil Aquilina <philaquilina@gmail.com>
2012-02-12 10:48:34 -0800

    setting up twitter omniauth

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '3.1.3'
 
 gem 'sqlite3'
 gem 'nifty-generators'
+gem 'omniauth'
+gem 'omniauth-twitter'
 
 
 # Gems used only for assets and not required
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -4,9 +4,9 @@ module ProductsHelper
   end
 
   def print_stock(stock, requested = 1)
-    if stock > 0
+    if stock > 0 && stock > requested
       return "<span class='in_stock'>In Stock (#{stock})</span>"
-    elsif stock > 0 && stock < requested
+    elsif stock > 0
       return "<span class='low_stock'>Insufficient Stock (#{stock})</span>"
     else
       return "<span class='out_stock'>Out of Stock</span>"
