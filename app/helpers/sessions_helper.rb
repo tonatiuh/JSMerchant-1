e188fde1707fe8ada7b143fa6e960429bd8b5d7d
Phil Aquilina <philaquilina@gmail.com>
2012-02-15 12:44:48 -0800

    added a redirect_back method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,15 @@
 module SessionsHelper
+
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+
+  def clear_return_to
+    session[:return_to] = nil
+  end
 end
